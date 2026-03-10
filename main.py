cfc236dd5b5deb94af25761fc524cd6468b708d7
Hash <d81465678@gmail.com>
2026-03-10 21:54:56 +0500

    Процедура страта успешна

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import asyncio
 import os
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
 from dotenv import load_dotenv
+
 
 # Эта команда ищет файл .env и загружает данные из него
 load_dotenv()
@@ -9,6 +12,88 @@ TOKEN = os.getenv("BOT_TOKEN")
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 # ... далее код бота ...
+
+@dp.message(CommandStart())
+async def cmd_start(message: types.Message):
+    await message.answer("Привет, я отсталый!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 async def main():
     print("Я запустился! Я отсталый!!!.") 
     await dp.start_polling(bot)
+if __name__ == "__main__":
+    try:
+        print("Пробую запустить бота...")
+        asyncio.run(main())
+    except Exception as e:
+        print(f"ПРОИЗОШЛА ОШИБКА: {e}")
