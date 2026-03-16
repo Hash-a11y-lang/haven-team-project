890eced48b188759fb23996ccd64233ac95de1b3
Hash <d81465678@gmail.com>
2026-03-16 23:44:35 +0500

    MOREEE BUTTONS

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
 from dotenv import load_dotenv
+from aiogram import F
 
 # Эта команда ищет файл .env и загружает данные из него
 load_dotenv()
@@ -16,25 +17,39 @@ dp = Dispatcher()
 async def cmd_start(message: types.Message):
     kb = [
         [
-            types.KeyboardButton(text="Звонки"),
-            types.KeyboardButton(text="Уроки")
-        ],
+            types.KeyboardButton(text="Понедельник"),
+            types.KeyboardButton(text="Вторник"),
+            types.KeyboardButton(text="Среда"),
+            types.KeyboardButton(text="Четверг"),
+            types.KeyboardButton(text="Пятница")
+        ]
     ]    
     keyboard = types.ReplyKeyboardMarkup(
         keyboard=kb,
         resize_keyboard=True,
         input_field_placeholder="Выберите расписание"
     )
-    await message.answer("Выберите тип расписания:", reply_markup=keyboard)
 
 
+@dp.message(F.text == "Понедельник")
+async def ring(message: types.Message):
+    await message.answer("Тут будут звоночки")
 
+@dp.message(F.text== "Вторник")
+async def lession(message: types.Message):
+    await message.answer("Вторник")
 
+@dp.message(F.text== "Среда")
+async def lession(message: types.Message):
+    await message.answer("Среда")
 
+@dp.message(F.text== "Четверг")
+async def lession(message: types.Message):
+    await message.answer("Четверг")
 
-
-
-
+@dp.message(F.text== "Пятница")
+async def lession(message: types.Message):
+    await message.answer("Пятница")
 
 
 
